bad5d35d77dd471ab2f45950e2893b3af539a27e
Leehyunji0715 <hjlee@bizflow.com>
2023-09-05 22:34:32 +0900

    update Chip and its style

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,19 +1,21 @@
 type Props = {
     text: string;
-    size?: 'normal' | 'small';
+    size?: 'normal' | 'small' | 'xSmall';
     round?: boolean;
     highlight?: boolean;
     fill?: boolean;
+    baseBackground?: boolean;
     icon?: React.ReactNode;
 };
 
-export default function Chip({text, icon, size='normal', round=false, highlight = true, fill=false}: Props) {
+export default function Chip({text, icon, size='normal', round=false, highlight = true, fill=false, baseBackground = false}: Props) {
     return (
         <div 
             data-highlight={highlight} 
             data-round={round} 
             data-size={size} 
-            data-fill={fill} 
+            data-fill={fill}
+            data-base-bg={baseBackground}
             className="chip"
         >
             {icon}
