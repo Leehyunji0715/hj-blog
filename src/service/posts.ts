cc43be0466a865082da87252a989f6f891ce28bd
Leehyunji0715 <hjlee@bizflow.com>
2023-09-06 22:28:13 +0900

    add service getting post count

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,6 +1,17 @@
 import { $Enums, Post } from "@prisma/client";
 import { prisma } from "./prisma";
 
+export async function postCount() {
+    return await prisma.post.groupBy({ by: 'category', _count: true})
+    .catch(async (e) => {
+        await prisma.$disconnect();
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
+}
+
 export async function getPosts(pageNo: number = 0, category?: $Enums.Category): Promise<Post[]> {
     const size = 12;
     const where = {
@@ -30,9 +41,9 @@ export async function getPosts(pageNo: number = 0, category?: $Enums.Category): 
 export async function addPost() {
     return await prisma.post.create({
         data: {
-          title: 'Test 6',
+          title: 'General 6',
           content: '# Heading level 1',
-          category: 'TEST',
+          category: 'GENERAL',
           published: true
         }
     })
