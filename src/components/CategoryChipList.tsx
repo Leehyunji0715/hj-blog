ef7e81668604fe6fbcf37967c7223ba41f6fb3ef
Leehyunji0715 <hjlee@bizflow.com>
2023-09-07 22:36:58 +0900

    add icon and category chip list

---
+++ src/components/CategoryChipList.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { $Enums } from "@prisma/client";
+import Chip from "./Chip";
+
+export const ALL = 'all';
+const categories = [ALL].concat(Object.values($Enums.Category));
+
+export default function CategoryChipList() {
+    const curCategory = useParams().category;
+    return (
+        <ul className="blog-category">
+            {categories.map(category => <li key={category}>
+                <Link href={`/blog/${category}?page=1`}>
+                    <Chip 
+                        text={category} 
+                        highlight={category === curCategory} 
+                        fill={category === curCategory}
+                        baseBackground
+                        round 
+                        size='small'
+                    />
+                </Link>
+            </li>)}
+        </ul>
+    )
+}
---
+++ src/components/icons/CalendarIcon.tsx
@@ -0,0 +1,5 @@
+import { AiOutlineCalendar } from 'react-icons/ai';
+
+export function CalendarIcon() {
+    return <AiOutlineCalendar/>;
+}
