89fbb38d00c58f2958cad7cb5c75eec59de2ebb3
Leehyunji0715 <hjlee@bizflow.com>
2023-09-14 21:53:18 +0900

    add google signIn callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,30 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { addUser, getUserRole } from "@/service/user";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
         })
-    ]
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            if (session.user && token.sub) {
+                session.user.role = (await getUserRole(token.sub ?? ''))?.role;
+            }
+            return session;
+        },
+        async signIn({ user: { id, name, email } }) {
+            if (!id || !email) {
+                return false;
+            }
+            await addUser({ id, name: name || '', email });
+            return true;
+        }
+
+    }
 };
 
 const handler = NextAuth(authOptions);
