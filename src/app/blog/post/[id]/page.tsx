237275152b62f0d797bff77d950597b78f05f01c
Leehyunji0715 <hjlee@bizflow.com>
2023-10-04 15:43:30 +0900

    add login session

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from "next/navigation";
-import { serialize } from "next-mdx-remote/serialize"
+import { getServerSession } from 'next-auth';
 import remarkGfm from "remark-gfm";
 import rehypePrettyCode from 'rehype-pretty-code';
+import { bundleMDX } from 'mdx-bundler';
 import MDXArticle from "@/components/mdx/MDXArticle";
 import { getPost } from "@/service/posts";
+import { EditIcon } from '@/components/icons';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { $Enums } from '@prisma/client';
 
 
 type Props = {
@@ -16,28 +21,35 @@ export default async function BlogPostPage({ params: {id} }: Props) {
     if (Number.isNaN(postId)) {
         redirect('/not-found');
     }
+    const session = await getServerSession(authOptions);
     const post = await getPost(postId);
     const imgSrc = post?.image ?? '/default_post_img.jpg';
     if (!post || !post.content) {
         redirect('/not-found');
     }
 
-    const mdxSource = await serialize(post.content, { 
-        parseFrontmatter: true,
-        mdxOptions: {
-            remarkPlugins: [remarkGfm],
-            rehypePlugins: [rehypePrettyCode],
-        }
-    })
+    const { code } = await bundleMDX({ 
+        source: post.content,
+        mdxOptions(options, frontmatter) {
+            options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm]
+            options.rehypePlugins = [...(options.rehypePlugins ?? []), rehypePrettyCode as any]
+            return options;
+        },
+    });
 
     return <div className='post'>
         <header className='post__header'>
-            <h1>[{post.category}] - {post.title}</h1>
+            <h1>
+                [{post.category}] - {post.title}
+                {
+                    session?.user.role === $Enums.Role.ADMIN && <Link href={`/post/update/${postId}`}><EditIcon/></Link> 
+                }
+            </h1>
             <time>{new Date(post.createdAt).toLocaleDateString()}</time>
-            <Image src={imgSrc} width={500} height={300} alt={`image of ${post.title}`}/>
+            <Image src={imgSrc} width={500} height={400} alt={`image of ${post.title}`}/>
         </header>
         <article>
-            <MDXArticle content={mdxSource} />
+            <MDXArticle code={code} />
         </article>
     </div>
 }
