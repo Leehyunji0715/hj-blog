a7664296e28b2f65cef497e79147340052b68d09
Leehyunji0715 <hjlee@bizflow.com>
2023-10-26 17:46:09 +0900

    add site metadata and apply it to themeprovider

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,9 +1,10 @@
 'use client';
-
 import { ThemeProvider as LibThemeProvider } from 'next-themes'
+import siteMetadata  from '@/data/siteMetadata'
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-    return <LibThemeProvider>
+    
+    return <LibThemeProvider defaultTheme={siteMetadata.theme} enableSystem>
         {children}
     </LibThemeProvider>
 }
---
+++ src/data/siteMetadata.ts
@@ -0,0 +1,24 @@
+/* reference from timlrx/tailwind-nextjs-starter-blog */
+
+const siteMetadata = {
+    title: 'Welcome to Hyunji\'s Blog',
+    author: 'Hyunji Lee',
+    headerTitle: 'Welcome to Hyunji\'s Blog',
+    description: 'A blog created with Next.js',
+    language: 'ko-kr',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://bearlee0715.com/',
+    siteRepo: 'https://github.com/Leehyunji0715/hj-blog',
+    siteLogo: '/static/images/logo.png',
+    // socialBanner: '/static/images/twitter-card.png',
+    // mastodon: 'https://mastodon.social/@mastodonuser',
+    // email: 'address@yoursite.com',
+    // github: 'https://github.com',
+    // twitter: 'https://twitter.com/Twitter',
+    // facebook: 'https://facebook.com',
+    // youtube: 'https://youtube.com',
+    // linkedin: 'https://www.linkedin.com',
+    locale: 'ko-KR'
+}
+
+export default siteMetadata;
