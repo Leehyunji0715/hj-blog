97b06524128fad21f2193c0cb73fa09c4bb529c5
Leehyunji0715 <hjlee@bizflow.com>
2023-10-27 17:13:39 +0900

    add postlistprovider and load init posts in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import SWRConfigContext from '@/context/SWRConfigContext';
 import ThemeProvider from '@/context/ThemeProvider';
 import AuthSessionProvider from '@/context/SessionProvider';
 import '../scss/main.scss';
+import { PostListProvider } from '@/context/PostListProvider';
+import { getAllPosts } from '@/service/posts';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,16 +18,19 @@ export const metadata: Metadata = {
 
 export type Theme = 'light' | 'dark';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  console.log("layout");
+  const posts = await getAllPosts();
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
         <AuthSessionProvider>
           <ThemeProvider>
+            <PostListProvider initValue={posts}>
             <Header/>
             <main>
               <SWRConfigContext>
@@ -33,6 +38,7 @@ export default function RootLayout({
               </SWRConfigContext>
             </main>
             <Footer/>
+            </PostListProvider>
           </ThemeProvider>
         </AuthSessionProvider>
       </body>
