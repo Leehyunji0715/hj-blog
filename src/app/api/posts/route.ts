4d31a00e8f53ccae90000952cd08979642eb15f7
Leehyunji0715 <hjlee@bizflow.com>
2023-10-29 20:58:57 +0900

    fix for build

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,9 @@
-import { getPosts } from "@/service/posts";
-import { $Enums } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { Category } from "@/model/Category";
+import { getPosts } from "@/service/posts";
 
 export async function GET(req: NextRequest) {
-    const category = req.nextUrl.searchParams.get('category') as $Enums.Category;
+    const category = req.nextUrl.searchParams.get('category') as Category;
 
     return getPosts()
         .then(posts => NextResponse.json(posts, { status: 200 }))
--- src/util/image.ts
+++ src/util/image.ts
@@ -1,12 +1,12 @@
-import { Post } from "@prisma/client";
+import { Post } from "@/service/posts";
 
 export function getImageSrcFrom(post: Post) {
-    const imageFile = post.imageFile ? Buffer.from(post.imageFile).toString('base64') : null;
-    if (imageFile) {
-        return `data:image/png;base64,${imageFile}`;
-    }
-    if (post.image) {
-        return post.image;
-    }
+    // const imageFile = post.imageFile ? Buffer.from(post.imageFile).toString('base64') : null;
+    // if (imageFile) {
+    //     return `data:image/png;base64,${imageFile}`;
+    // }
+    // if (post.image) {
+    //     return post.image;
+    // }
     return '/default_post_img.jpg';
 }
