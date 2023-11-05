6c522bfad8574397b7edf2fb1df1345c4613f2a5
Leehyunji0715 <hjlee@bizflow.com>
2023-11-05 16:50:32 +0900

    remove swr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,18 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import SWRConfigContext from '@/context/SWRConfigContext';
 import ThemeProvider from '@/context/ThemeProvider';
 import '../scss/main.scss';
 
 const inter = Inter({ subsets: ['latin'], fallback: ['system-ui', 'arial'] });
 
 export const metadata: Metadata = {
-  title: `이현지's 블로그`,
+  title: {
+    default: "이현지's 블로그",
+    template: "케이티의 블로그 | %s"
+  },
   description: '반갑습니다! 이현지 블로그 입니다😊',
+  icons: 'favicon.ico'
 }
 
 export type Theme = 'light' | 'dark';
@@ -26,9 +29,7 @@ export default function RootLayout({
           <ThemeProvider>
             <Header/>
             <main>
-              <SWRConfigContext>
-                  {children}
-              </SWRConfigContext>
+              {children}
             </main>
             <Footer/>
           </ThemeProvider>
