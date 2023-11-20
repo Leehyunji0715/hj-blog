bbb7587ce17599bd31900f128c363c85dfbde07e
Leehyunji0715 <hjlee@bizflow.com>
2023-11-20 14:47:37 +0900

    update profile image and add name hightlihgt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,13 +30,12 @@ export default function Home() {
     <div className='home d-flex flex-col'>
       <section className='section-profile'>
         <div className='profile-text'>
-          <h1>{`Hello, I'm Hyunji`}</h1>
+          <h1>Hello, {`I'm`} <span className='text-highlight'>Hyunji</span></h1>
           <h2 className='mt-md'>Frontend Developer</h2>
           <h3 className='mt-sm' >Enjoying new experience and learning</h3>
         </div>
         <div className='profile-img'>
-          <Image priority className='profile-img-bg' src='/profile_bg.svg' fill alt='profile background'/>
-          <Image priority className='profile-img-main' src='/sample_profile.png' fill alt='profile'/>
+          <Image priority className='profile-img-main' src='/images/site/profile.svg' fill alt='profile image'/>
         </div>
       </section>
       <SectionBox title='Skills' className='home__skill'>
