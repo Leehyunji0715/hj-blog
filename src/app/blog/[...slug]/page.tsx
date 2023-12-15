e87e7913f86b862cc0ec4c6721ae9bebd10767c2
Leehyunji0715 <hjlee@bizflow.com>
2023-12-15 15:19:37 +0900

    get post counts by category, and show only existing posts' categories

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import CategoryChipList from "@/components/CategoryChipList";
 import GridPostList from "@/components/GridPostList";
 import PostPaginator from "@/components/PostPaginator";
 import { Category } from "@/model/Category";
-import { getPosts } from "@/service/posts";
+import { getPostCount, getPosts } from "@/service/posts";
 import { genPageMetadata } from "@/util/seo";
 
 type Props = {
@@ -13,7 +13,8 @@ export const metadata = genPageMetadata({ title: 'Blog' });
 
 export const dynamic = 'force-static';
 export async function generateStaticParams() {
-    return Object.values(Category).map((category) => ([ category, 1 ]))
+    const count = await getPostCount();
+    return [Category.ALL, ...Object.keys(count)].map((category) => ([category, 1]))
 }
 
 const ALL = 'all';
@@ -21,8 +22,9 @@ const POSTS_PER_PAGE = 12;
 
 const isInRange = (pageNo: number, i: number) => POSTS_PER_PAGE * (pageNo - 1) <= i && i < POSTS_PER_PAGE * pageNo;
 
-export default async function BlogPageByCategory({ params: {slug}}: Props) {
-    const categories = Object.values(Category);
+export default async function BlogPageByCategory({ params: { slug } }: Props) {
+    const count = await getPostCount();
+    const categories = [Category.ALL, ...Object.keys(count)];
     const category = slug[0] as Category;
     const pageNo = Number(slug[1]);
     const posts = await getPosts();
@@ -33,13 +35,12 @@ export default async function BlogPageByCategory({ params: {slug}}: Props) {
             totalCount++;
             return p.category === category && isInRange(pageNo, i)
         });
-    
-    
+
     return <div className="blog">
-        <CategoryChipList currentCategory={category} categories={categories}/>
+        <CategoryChipList currentCategory={category} categories={categories} />
         <div className="blog__post-list">
-            { displayedPosts.length ? <GridPostList posts={displayedPosts}/> : "No Post"}
+            {displayedPosts.length ? <GridPostList posts={displayedPosts} /> : "No Post"}
         </div>
-        <PostPaginator total={totalCount} itemsPerPage={POSTS_PER_PAGE}/>
+        <PostPaginator total={totalCount} itemsPerPage={POSTS_PER_PAGE} />
     </div>;
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,42 +1,50 @@
-import path from "path";
-import { cache } from "react";
-import { promises as fs } from "fs";
+import path from 'path'
+import { cache } from 'react'
+import { promises as fs } from 'fs'
 
 export type Post = {
-    title: string;
-    description: string;
-    date: Date;
-    category: string;
-    path: string;
-    image: string;
-    published: boolean;
-};
-
-export type PostData = Post & { content: string, next: Post | null, prev: Post | null };
-
-export const getPosts = cache(async () => {
-    const filePath = path.join(process.cwd(), 'src/data', 'posts.json');
-    return fs.readFile(filePath, 'utf-8')
-        .then<Post[]>(JSON.parse)
-        .then((posts) => posts.filter(p =>p.published))
-        .then((posts) => posts.sort((a, b) => a.date > b.date ? -1 : 1));
-});
-
-export async function getPost(path: string): Promise<Post | undefined> {
-    const posts = await getPosts();
-    return posts.find((item) => item.path === path);
+  title: string
+  description: string
+  date: Date
+  category: string
+  path: string
+  image: string
+  published: boolean
 }
 
-export async function getPostContent(fileName: string): Promise<PostData>  {
-    const filePath = path.join(process.cwd(), 'src/data', 'posts', `${fileName}.mdx`);
-    const posts = await getPosts();
-    const post = posts.find(post => post.path === fileName);
+export type PostData = Post & { content: string; next: Post | null; prev: Post | null }
 
-    if (!post)  throw new Error(`${fileName}에 해당하는 내용을 찾을 수 없음`);
-    const index = posts.indexOf(post);
-    const prev = index > 0 ? posts[index - 1]: null;
-    const next = index < posts.length - 1 ? posts[index + 1] : null;
-    const content = await fs.readFile(filePath, 'utf-8');
+export const revalidate = 12 * 3600 // 12 hours
 
-    return { next, prev, content, ...post };
+export const getPostCount = cache(async () => {
+  const filePath = path.join(process.cwd(), 'src/data', 'count.json')
+  return fs.readFile(filePath, 'utf-8').then(JSON.parse)
+})
+
+export const getPosts = cache(async () => {
+  const filePath = path.join(process.cwd(), 'src/data', 'posts.json')
+  return fs
+    .readFile(filePath, 'utf-8')
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.filter((p) => p.published))
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)))
+})
+
+export async function getPost(path: string): Promise<Post | undefined> {
+  const posts = await getPosts()
+  return posts.find((item) => item.path === path)
+}
+
+export async function getPostContent(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), 'src/data', 'posts', `${fileName}.mdx`)
+  const posts = await getPosts()
+  const post = posts.find((post) => post.path === fileName)
+
+  if (!post) throw new Error(`${fileName}에 해당하는 내용을 찾을 수 없음`)
+  const index = posts.indexOf(post)
+  const prev = index > 0 ? posts[index - 1] : null
+  const next = index < posts.length - 1 ? posts[index + 1] : null
+  const content = await fs.readFile(filePath, 'utf-8')
+
+  return { next, prev, content, ...post }
 }
